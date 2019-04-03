78355cb85ccfa6a363469f632156e9e657a25b85
jiangtingwen <18720573559@163.com>
2019-04-03 17:04:56 +0800

    add new file

---
+++ codewars/demo1/index.js
@@ -0,0 +1,16 @@
+function generateHashtag(str){
+    // if(str.length>140||str==='')
+    //     return false;
+       
+    //     str=str.split(' ').map(word=>word.charAt(0).toUpperCase()+word.slice(1)).join(' ');
+    //     str='#'+str;
+    //     return str;
+    return str.length>140||str===''? false: 
+    '#'+str.split(' ').map(capitalize).join(' ')
+}
+    function capitalize (word){
+        return word.charAt(0).toUpperCase()+word.slice(1);
+        console.log(word);
+    }
+
+ console.log(generateHashtag('hello miss dong'));
