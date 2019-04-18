d489af4ec36740ba8a26a2f89aaa1f9d30355caa
jiangtingwen <18720573559@163.com>
2019-04-18 18:58:03 +0800

    add new day study file

---
+++ canvas/arc/round_item.js
@@ -0,0 +1,31 @@
+/**
+ * @author 滴滴
+ * @desc 画星
+ * 一个功能一个类  封装 一个类一个文件
+ * 多人协作 模块化  
+ * 业务代码  要抽离， 封装
+ */
+function RoundItem(index,x,y,ctx) {
+    this.index = index;
+    this.y = y;
+    this.x = x;
+    this.ctx = ctx;
+    this.r = Math.random()*2+1;  //半径
+    let alpha =(Math.floor( Math.random()*10)+1)/10/2;
+    this.color = `rgb(255,255,255,${alpha})`;
+
+
+}
+RoundItem.prototype.draw=function(){
+    this.ctx.fillStyle = this.color;
+    this.ctx.shadowblur=this.r*2;
+    this.ctx.beginPath();
+    this.ctx.arc(this.x,this.y,this.r,0,2*Math.PI,false);
+    this.ctx.closePath();
+    this.ctx.fill();
+
+}
+RoundItem.prototype.move = function(){
+    this.y-=0.15;
+    this.draw();
+}
