4f4420bee9b6be8f5abd574e8fee63030b4af5bf
jiangtingwen <18720573559@163.com>
2019-04-26 17:02:25 +0800

    add new file

---
+++ APP/sg/config/api.js
@@ -0,0 +1,5 @@
+export const API_BASE = 'https://www.easy-mock.com/mock/5cbeb468d2955f5e587f936a/www.jtw/jiang'
+export const API_DETAIL = 'https://www.easy-mock.com/mock/5cbeb468d2955f5e587f936a/www.jtw/ting'
+export const API_CREATE = 'https://www.easy-mock.com/mock/5af03ac5957fd4308a30b9c0/api/create#!method=get'
+export const API_DELETE_IMG = 'https://www.easy-mock.com/mock/5af03ac5957fd4308a30b9c0/api/delete_img#!method=delete'
+export const API_LOGIN = 'https://www.easy-mock.com/mock/5af03ac5957fd4308a30b9c0/api/login#!method=post'
--- codewars/建塔/index.js
+++ codewars/建塔/index.js
@@ -20,9 +20,18 @@
 //   ' ********* ', 
 //   '***********'
 // ]
-function towerBuilder(n){
-    
+function towerBuilder(nFloors){
+    var tower = [];
+    for (var i = 0; i < nFloors; i++) {
+      tower.push(" ".repeat(nFloors - i - 1)
+               + "*".repeat((i * 2)+ 1)
+               + " ".repeat(nFloors - i - 1));
+    }
+    return tower;
+  
+
+    }
 
     
-}
-console.log(towerBuilder(3));
+
+console.log(towerBuilder(9));
