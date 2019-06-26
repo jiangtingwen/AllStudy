75e5574a6464428881343dbbcbb6f466475b9767
jiangtingwen <18720573559@163.com>
2019-06-26 12:02:48 +0800

    add new files

---
+++ vue-learn/vue-chart-demo/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+import {getToken} from '@/utils/auth'
+// 当进入/  如果没有token 就去登录 ?
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/pages/index')
+    },
+    {
+      path:'/login',
+      name:'login',
+      component: () => import('@/pages/login')
+    },
+
+  ]
+})
+let token = getToken();
+const whiteList = ['/login','signup','/auth-redirect']
+router.beforeEach((to,from,next)=>{
+  if(token){
+
+  }else {
+    console.log(to.path);
+    if(whiteList.includes(to.path)){
+      next();
+    }else{
+      next(`/login?redirect=${to.path}`);
+    }
+  }
+})
+export default router;
+// to from $router path /
