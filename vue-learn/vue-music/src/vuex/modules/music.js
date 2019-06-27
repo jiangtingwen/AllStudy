499a53cf1a98b08e224e0a13f77f288e3cb7cd27
jiangtingwen <18720573559@163.com>
2019-06-27 17:02:11 +0800

    add new file

--- vue-learn/vue-music/src/vuex/modules/music.js
+++ vue-learn/vue-music/src/vuex/modules/music.js
@@ -40,11 +40,38 @@ const actions = {
             playlist = [...playlist,song]
             currentIndex = playlist.length - 1
         }
-        commit()
+        commit(types.SET_PLAY_LIST,playlist)
+        commit(types.SET_CURRENT_INDEX,currentIndex)
+        commit(types.SET_PLAYING,true)
+    },
+    //加入播放列表
+    addPlayList ({commit,state},song) {
+        let playList = [...state.playList.slice(),song]
+        if(playList.length === 1) {
+            let currentIndex = state.currentIndex
+            currentIndex++
+            commit(types.SET_CURRENT_INDEX,currentIndex)
+            commit(types.SET_PLAYING,true)
+        }
+        commit(types.SET_PLAY_LIST,playList)
+    },
+    //保存播放历史
+    savePlayHistory ({commit,state},song) {
+        let playHistory = state.playHistory.slice()
+        playHistory = [...playHistory,song]
+        commit(types.SAVE_PLAY_HISTORY,playHistory)
+
     }
 }
 const getters = {
-
+        playing: state => state.playing,
+        playList:state => state.playList,
+        currentIndex:state => state.currentIndex,
+        currentSong: state => {
+            return state.playList[state.currentIndex] || {}
+        },
+        favoriteList:state => state.favoriteList,
+        playHistory:state => state.playHistory
 }
 export default {
     state,
