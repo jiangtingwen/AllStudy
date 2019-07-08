e5ae132faad2be5f9aabe8744132cc5e636cd3ca
jiangtingwen <18720573559@163.com>
2019-07-08 10:06:37 +0800

    add new file

---
+++ vue-learn/aiQiYi/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <!-- tab导航栏 -->
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import header from "@/components/header"
+export default {
+  name: "App",
+
+  components:{
+    "v-header":header
+  }
+}
+</script>
+
+<style lang="stylus">
+@import "./assets/css/function"
+@font-face 
+  font-family: 'iconfont'  /* project id 1280431 */
+  src: url('//at.alicdn.com/t/font_1280431_41riqh2trid.eot')
+  src: url('//at.alicdn.com/t/font_1280431_41riqh2trid.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_1280431_41riqh2trid.woff2') format('woff2'),
+  url('//at.alicdn.com/t/font_1280431_41riqh2trid.woff') format('woff'),
+  url('//at.alicdn.com/t/font_1280431_41riqh2trid.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_1280431_41riqh2trid.svg#iconfont') format('svg')
+
+.iconfont
+    font-family:"iconfont" !important
+    font-size:16px
+    font-style:normal
+    -webkit-font-smoothing: antialiased
+    -webkit-text-stroke-width: 0.2px
+    -moz-osx-font-smoothing: grayscale
+html,body
+  line-height 1
+  font-family PingFang SC, STHeitiSC-Light, Helvetica-Light, arial, sans-serif
+  user-select none
+  -webkit-tap-highlight-color transparent
+  background rgba(8, 5, 58, 0.9)
+  color #fff
+</style>
---
+++ vue-learn/aiQiYi/src/components/header.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="tab">
+      <div class="left-icon" @click="leftEvent">
+          <i class="iconfont">&#xe61e;</i>
+      </div>
+      <div class="content-tab">
+         <router-link :class="objectClass" to="/my">
+             <span class="tab-link" >我</span>
+         </router-link>
+         <router-link class="tab_item" to="/listen">
+             <span class="tab-link">听</span>
+         </router-link>
+         <router-link class="tab_item" to="/look">
+             <span class="tab-link">看</span>
+         </router-link>
+         <router-link class="tab_item" to="/sing">
+             <sapn class="tab-link">唱</sapn>
+         </router-link>
+      </div>
+      <div class="right-icon" @click="Add">
+          <i class="iconfont">&#xe601;</i>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'hd',
+    data () {
+            return {
+                objectClass: {
+                    tab_item:false,
+                    tab_item_big:true
+
+                }
+            }
+        
+        }
+    }
+
+</script>
+
+<style lang="stylus" scoped>
+@import '../assets/css/function'
+.tab
+    padding px2rem(50px) px2rem(30px)
+    display flex
+    .left-icon
+        flex 0 0 px2rem(50px)
+    .content-tab
+        flex 1
+        text-align center
+        .tab_item
+           text-decoration none
+           color #59d2d3
+           margin 0 20px
+        .tab_item_big
+           text-decoration none
+           color #59d2d3
+           margin 0 20px
+           font-size 20px
+           text-shadow #ffffff 2px 0 10px
+
+    .right-icon
+        flex 0 0 px2rem(50px)
+
+
+
+</style>
